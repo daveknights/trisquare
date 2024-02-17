5f6cbcf513d5c1e66f1280b51c0956eb455c23e2
David Knights <david.knights@vcars.co.uk>
2024-02-17 15:24:04 +0000

    Create IconButton component

---
+++ components/IconButton.js
@@ -0,0 +1,29 @@
+import { StyleSheet, TouchableOpacity, Image } from 'react-native';
+import colours from '../defaults/colours';
+
+export default function IconButton({ path, bgColour, onPress }) {
+    return (
+        <TouchableOpacity style={{...styles.button, backgroundColor: colours[bgColour]}} onPress={onPress}>
+            <Image
+                style={styles.buttonIcon}
+                source={path} />
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    button: {
+        borderColor: colours.primary,
+        borderRadius: 15,
+        borderWidth: 1,
+        marginBottom: 30,
+        paddingBottom: 15,
+        paddingLeft: 50,
+        paddingRight: 50,
+        paddingTop: 15,
+    },
+    buttonIcon: {
+        height: 24,
+        width: 24,
+    },
+});
