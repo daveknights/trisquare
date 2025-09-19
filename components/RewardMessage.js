2196529e668ab41057db8b7fca9d502fc0e39209
daveknights <dave.knights@virginmedia.com>
2025-09-19 19:27:26 +0100

    Adjust reward message vertical position

--- components/RewardMessage.js
+++ components/RewardMessage.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flex: 1,
         justifyContent: 'center',
-        paddingTop: 30,
+        paddingTop: 50,
     },
     heading: {
         backgroundColor: colour.style.green,
